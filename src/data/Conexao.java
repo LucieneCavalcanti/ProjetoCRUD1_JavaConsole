ae8dfb6fee8e6755cdb17a862486ee8f0dcfbee8
LucieneCavalcanti <lucienecavalcanti@live.com>
2024-08-20 08:09:28 -0300

    20082024

--- src/data/Conexao.java
+++ src/data/Conexao.java
@@ -1,14 +1,18 @@
 package data;
 import java.sql.Connection;
+import java.sql.DriverManager;
 public class Conexao {
     private Connection conexao;
     public Connection getConexao(){
         return conexao;
     }
-    public Conexao(){
+    public Conexao() throws Exception{
         String url="jdbc:sqlserver://LAB01-MAQ18:1433;databaseName=bdJava;trustServerCertificate=true;encript=false";
         String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
         String usuario="aluno";
         String senha="dbo";
+        Class.forName(driver);
+        conexao = DriverManager.getConnection
+        (url, usuario, senha);
     }
 }
