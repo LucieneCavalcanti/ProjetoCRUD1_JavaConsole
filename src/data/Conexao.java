0fb011b8b8090e1ec4a5ce4111e8c9c3d5182cd5
LucieneCavalcanti <lucienecavalcanti@live.com>
2024-08-28 08:49:18 -0300

    Mysql

--- src/data/Conexao.java
+++ src/data/Conexao.java
@@ -7,10 +7,12 @@ public class Conexao {
         return conexao;
     }
     public Conexao() throws Exception{
-        String url="jdbc:sqlserver://LAB01-MAQ18:1433;databaseName=bdJava;trustServerCertificate=true;encript=false";
-        String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-        String usuario="aluno";
-        String senha="dbo";
+        //String url="jdbc:sqlserver://LAB01-MAQ18:1433;databaseName=bdJava;trustServerCertificate=true;encript=false";
+        String url="jdbc:mysql://localhost:3306/bdJava?zeroDateTimeBehavior=CONVERT_TO_NULL";
+        //String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        String driver="com.mysql.cj.jdbc.Driver";
+        String usuario="root";//"aluno";
+        String senha="%Fatecrp!";//"dbo";
         Class.forName(driver);
         conexao = DriverManager.getConnection
         (url, usuario, senha);
